eb68c9a5b96e78cff5692298270558249863a139
Ali Aibolatov <ali.a@1fit.app>
2024-10-08 17:20:24 +0500

    Initial Commit

---
+++ TzApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TzApp
+//
+//  Created by Али Айболатов on 08.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TzApp/TzAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TzAppApp.swift
+//  TzApp
+//
+//  Created by Али Айболатов on 08.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TzAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
